ec0b1d504240f65cfd76ebc91175586da2e35695
Steven Milne <steven@digitaldelivery.co.uk>
2017-02-25 14:53:44 +0000

    oops

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 // Import the client event constants from the Slack API
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 
-const token = 'xoxb-145771162305-MnZxDPZhjvJnMowUIX5UNYBm';
+const token = '';
 
 // The Slack constructor takes 2 arguments:
 // token - String representation of the Slack token
